2e4d538eab1a59985db0c58c8b49eeece7a9fe0d
tango600 <tango600@mail.ru>
2016-02-15 23:12:28 +0300

    DCLLancher v 1.1

---
+++ Launcher/Add.pas
@@ -0,0 +1,105 @@
+unit Add;
+
+{$IFDEF FPC}
+  {$MODE Delphi}
+{$ENDIF}
+
+interface
+
+uses
+{$IFnDEF FPC}
+  Windows,
+{$ELSE}
+  LCLIntf, LCLType, LMessages,
+{$ENDIF}
+  Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, ExtCtrls, ComCtrls, uBaseesStorage;
+
+type
+
+  { TfAddBase }
+
+  TfAddBase=class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    StatusBar1: TStatusBar;
+    leNameBase: TLabeledEdit;
+    lePathIni: TLabeledEdit;
+    SpeedButton1: TSpeedButton;
+    btOK: TButton;
+    bkCancel: TButton;
+    OpenDialog1: TOpenDialog;
+    leParams: TLabeledEdit;
+    procedure btOKClick(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure bkCancelClick(Sender: TObject);
+  private
+    Params:RBaseParams;
+  public
+    EditMode: TEditActions;
+    BaseName: String;
+  end;
+
+
+implementation
+
+
+{$R *.dfm}
+
+procedure TfAddBase.btOKClick(Sender: TObject);
+begin
+  Case EditMode of
+  teaAdding:Begin
+    Params.Title:=leNameBase.Text;
+    Params.IniPath:=lePathIni.Text;
+    Params.Params:=leParams.Text;
+    Params.UID:='';
+
+    AddBaseToIni(Path+'Bases.ini', Params);
+  End;
+  teaEditing:Begin
+    Params.Title:=leNameBase.Text;
+    Params.IniPath:=lePathIni.Text;
+    Params.Params:=leParams.Text;
+    Params.UID:='';
+
+    EditBaseIni(Path+'Bases.ini', Params);
+  End;
+  End;
+  Self.Close;
+end;
+
+procedure TfAddBase.SpeedButton1Click(Sender: TObject);
+begin
+  If OpenDialog1.Execute then
+    lePathIni.Text:=OpenDialog1.FileName;
+end;
+
+procedure TfAddBase.FormShow(Sender: TObject);
+begin
+  Case EditMode of
+  teaAdding:
+    Begin
+      leNameBase.Clear;
+      lePathIni.Clear;
+      leParams.Clear;
+    End;
+    teaEditing:
+    Begin
+      Params:=GetSection(Path+'Bases.ini', BaseName);
+      leNameBase.Text:=Params.Title;
+      lePathIni.Text:=Params.IniPath;
+      leParams.Text:=Params.Params;
+    End;
+  End;
+  leNameBase.SetFocus;
+end;
+
+procedure TfAddBase.bkCancelClick(Sender: TObject);
+begin
+  Self.Close;
+end;
+
+
+end.
---
+++ Launcher/ubaseesstorage.pas
@@ -0,0 +1,177 @@
+unit uBaseesStorage;
+
+interface
+
+uses
+{$IFDEF FPC}
+  LConvEncoding,
+{$ENDIF}
+  Classes, SysUtils, IniFiles;
+
+type
+  RBaseParams=Record
+    Title, IniPath, Params, UID: String;
+  end;
+
+  TEditActions=(teaNone, teaEditing, teaAdding, teaDeleting);
+
+  TBaseParams=Array of RBaseParams;
+
+ procedure ReadIni(IniFileName:String; var BaseParams:TBaseParams);
+ function GetSection(IniFileName, SectionName:String):RBaseParams;
+ procedure AddBaseToIni(IniFileName:String; Params:RBaseParams);
+ Function GetBaseParam(Index: Integer; var BaseParams:TBaseParams): String;
+ procedure EditBaseIni(IniFileName:String; Params:RBaseParams);
+ procedure DeleteBase(IniFileName, SectionName:String);
+ function GetBaseUID(IniFileName, SectionName:String):String;
+
+ function NormalizeEncoding(const Encoding: string): string;
+
+ function ToInterface(S:String):String;
+ function ToSystem(S:String):String;
+
+var
+  Path, DefaultSystemEncoding:String;
+
+const
+{$IFDEF DELPHI}
+  EncodingUTF8='utf8';
+  UTF8BOM=#$EF#$BB#$BF;
+  UTF16LEBOM=#$FF#$FE;
+  DefaultInterfaceEncoding='cp1251';
+{$ENDIF}
+{$IFDEF FPC}
+  DefaultInterfaceEncoding=EncodingUTF8;
+{$ENDIF}
+
+
+implementation
+
+
+function NormalizeEncoding(const Encoding: string): string;
+var
+  i: Integer;
+begin
+  Result:=Trim(LowerCase(Encoding));
+  if Pos('win', Result)=1 then
+  begin
+    Delete(Result, 1, 3);
+    Result:='cp'+Result;
+  end;
+  for i:=length(Result) downto 1 do
+    if Result[i]='-' then System.Delete(Result,i,1);
+end;
+
+function ToInterface(S:String):String;
+begin
+  Result:=ConvertEncoding(S, DefaultSystemEncoding, DefaultInterfaceEncoding);
+end;
+
+function ToSystem(S:String):String;
+begin
+  Result:=ConvertEncoding(S, DefaultInterfaceEncoding, DefaultSystemEncoding);
+end;
+
+Function GetBaseParam(Index: Integer; var BaseParams:TBaseParams): String;
+Begin
+  If Length(BaseParams)>0 then
+  Begin
+    If Index<> - 1 then
+    Begin
+      Result:='-ini "'+BaseParams[Index].IniPath+'"';
+      If BaseParams[Index].Params<>'' then
+        Result:=Result+' '+BaseParams[Index].Params;
+    end
+    Else
+      Result:='';
+  end
+  Else
+    Result:='';
+end;
+
+procedure ReadIni(IniFileName:String; var BaseParams:TBaseParams);
+var
+  SectionNum: Word;
+  Sections:TStringList;
+  Ini:TIniFile;
+begin
+  Ini:=TIniFile.Create(IniFileNAme);
+  Sections:=TStringList.Create;
+
+  Ini.ReadSections(Sections);
+  SetLength(BaseParams, Sections.Count);
+  For SectionNum:=1 to Sections.Count do
+  Begin
+    BaseParams[SectionNum-1].Title:=ToInterface(Sections[SectionNum-1]);
+    BaseParams[SectionNum-1].IniPath:=ToInterface(Ini.ReadString(Sections[SectionNum-1], 'IniFile', ''));
+    BaseParams[SectionNum-1].Params:=ToInterface(Ini.ReadString(Sections[SectionNum-1], 'Params', ''));
+    BaseParams[SectionNum-1].UID:=Ini.ReadString(Sections[SectionNum-1], 'BaseUID', '');
+  End;
+  Ini.Free;
+end;
+
+function GetSection(IniFileName, SectionName:String):RBaseParams;
+var
+  Ini:TIniFile;
+begin
+  Ini:=TIniFile.Create(IniFileName);
+
+  Result.Title:=ToInterface(Ini.ReadString(ToSystem(SectionName), 'Title', ''));
+  Result.IniPath:=ToInterface(Ini.ReadString(ToSystem(SectionName), 'IniFile', ''));
+  Result.Params:=ToInterface(Ini.ReadString(ToSystem(SectionName), 'Params', ''));
+  Result.UID:=Ini.ReadString(ToSystem(SectionName), 'BaseUID', '');
+
+  Ini.Free;
+end;
+
+procedure AddBaseToIni(IniFileName:String; Params:RBaseParams);
+var
+  Ini:TIniFile;
+begin
+  Ini:=TIniFile.Create(IniFileName);
+
+  Ini.WriteString(ToSystem(Params.Title), 'Title', ToSystem(Params.Title));
+  Ini.WriteString(ToSystem(Params.Title), 'IniFile', ToSystem(Params.IniPath));
+  Ini.WriteString(ToSystem(Params.Title), 'Params', ToSystem(Params.Params));
+  Ini.WriteString(ToSystem(Params.Title), 'BaseUID', Params.UID);
+
+  Ini.Free;
+end;
+
+procedure EditBaseIni(IniFileName:String; Params:RBaseParams);
+var
+  Ini:TIniFile;
+begin
+  Ini:=TIniFile.Create(IniFileName);
+
+  Ini.EraseSection(ToSystem(Params.Title));
+  Ini.WriteString(ToSystem(Params.Title), 'Title', ToSystem(Params.Title));
+  Ini.WriteString(ToSystem(Params.Title), 'IniFile', ToSystem(Params.IniPath));
+  Ini.WriteString(ToSystem(Params.Title), 'Params', ToSystem(Params.Params));
+  Ini.WriteString(ToSystem(Params.Title), 'BaseUID', Params.UID);
+
+  Ini.Free;
+end;
+
+procedure DeleteBase(IniFileName, SectionName:String);
+var
+  Ini:TIniFile;
+begin
+  Ini:=TIniFile.Create(IniFileName);
+
+  Ini.EraseSection(ToSystem(SectionName));
+
+  Ini.Free;
+end;
+
+function GetBaseUID(IniFileName, SectionName:String):String;
+var
+  Ini:TIniFile;
+begin
+  Ini:=TIniFile.Create(IniFileName);
+  Result:=Ini.ReadString(ToSystem(SectionName), 'BaseUID', '');
+  Ini.Free;
+end;
+
+end.
+
